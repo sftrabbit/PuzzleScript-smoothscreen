54b386b172174277882e40f4211709e9930e1071
Stephen Lavelle <analytic@gmail.com>
2021-02-03 13:46:06 +0100

    moving everything to src directory

---
+++ tests/resources/wrapper.js
@@ -0,0 +1,31 @@
+var lastDownTarget = null;
+var canvas = null;
+var input = document.createElement('TEXTAREA');
+
+function canvasResize() {
+
+}
+
+function redraw() {
+
+}
+
+function forceRegenImages(){
+
+}
+
+var levelString;
+var inputString;
+var outputString;
+
+function consolePrintFromRule(text){}
+function consolePrint(text) {}
+function consoleError(text) {
+//	window.console.log(text);
+}
+
+function consoleCacheDump() {}
+var editor = {
+	getValue : function () { return levelString }
+}
+
